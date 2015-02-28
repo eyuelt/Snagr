715dba798f151c9ce34b5e6a31524a9e87aa3325
Alexander Hsu <pathofwind@gmail.com>
2015-02-28 13:06:55 -0800

    Initial Commit

---
+++ Snagr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snagr
+//
+//  Created by Alexander Hsu on 2/28/15.
+//  Copyright (c) 2015 Alexander Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
